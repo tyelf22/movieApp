162e5a22e686d57f3df693e2a64c903f5e72ab2c
Tyson <tyelf22@hotmail.com>
2019-01-03 00:16:09 -0800

    initial commit

---
+++ js/main.js
@@ -0,0 +1,33 @@
+$(document).ready(() => {
+    $("#searchForm").on("submit", (e) => {
+       let searchText =  $('#searchText').val();
+       getMovies(searchText);
+        e.preventDefault();
+    })
+});
+
+function getMovies(searchText){
+    axios.get('http://www.omdbapi.com/?apikey=thewdb&s='+searchText)
+        .then((response) => {
+            console.log(response);
+            let movies = response.data.Search;
+            let output = "";
+            $.each(movies, (index, movie) =>{
+                output += `
+                <div class="col-md-3">
+                   <div class="card text-center">
+                     <img src="${movie.Poster}">
+                        <h5>${movie.Title}</h5>
+                     <a onclick="movieSelected('${movie.imdbID}')" class="btn btn-primary" href="#">Movie Details</a>
+                   </div>
+                </div>
+                `;
+            })
+
+            $('#movies').html(output);
+
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+}
